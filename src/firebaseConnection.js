15d218420527ee3e7d4be2138fa55feec994dfe7
davidGBbr <davidgomesbrigido@gmail.com>
2023-11-07 14:28:02 -0300

    Configurando firebase

---
+++ src/firebaseConnection.js
@@ -0,0 +1,16 @@
+import { initilizeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRIY9z_EX-x73J_gVjhcWwag7XLG1Fg9g",
+  authDomain: "curso-fullstack-pro.firebaseapp.com",
+  projectId: "curso-fullstack-pro",
+  storageBucket: "curso-fullstack-pro.appspot.com",
+  messagingSenderId: "985068799185",
+  appId: "1:985068799185:web:0beb2119f7cd639ca331f0",
+  measurementId: "G-5XK3G96YY8",
+};
+
+const firebaseApp = initilizeApp(firebaseConfig);
+
+export const db = getFirestore(firebaseApp);
